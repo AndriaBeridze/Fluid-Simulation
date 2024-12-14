ba63af783b953391f0c8bb79e2a39e9acf9df158
AndriaBeridze <andria24b@gmail.com>
2024-12-13 20:01:43 -0500

    Set up .NET console

---
+++ Scripts/Framework/App/Core/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Fluid.App;
+
+public static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Fluid Simulation By Andria Beridze! So Cool...");
+    }
+}
